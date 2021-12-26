df23cccf810f544d32b88fa7fb0f590bd64b7f7a
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2021-12-26 12:31:50 +0530

    WeirdAlgorithm
    Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
    3→10→5→16→8→4→2→1
    
    Your task is to simulate the execution of the algorithm for a given value of n.

---
+++ WeirdAlgorithm.py
@@ -0,0 +1,10 @@
+def nu(n):
+    while n !=1:
+        print(n,end=" ")
+        if n%2 == 0:
+            n = n//2
+        else:
+            n=(n*3)+1
+    print(1,end=" ")        
+
+nu(int(input()))          
